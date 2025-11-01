6fd9e674a7e682fcf6ceae576b6b32c7e091e37e
msn9189 <mehdizadeh.sn@gmail.com>
2025-11-01 20:24:17 +0000

    solidity version changed

--- contracts/DiaryNFT.sol
+++ contracts/DiaryNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 // Import Openzeppelin's ERC721 contract for standard NFT functionality.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";

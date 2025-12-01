c69926b1e85b8bdd09c76bb8b0a7f0e43aa2fb69
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-01 19:14:52 +0000

    variabled defined

--- contracts/MiniBaseName.sol
+++ contracts/MiniBaseName.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.24;
 
 contract MiniBaseName {
+    mapping(string => address) public nameToAddr; 
+    mapping(address => string) public addrToName;
 
+    uint256 public constant PRICE = 0.00001 ether;
+
+    event Registered(string name, address owner);
 }

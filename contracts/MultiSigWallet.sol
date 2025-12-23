5fe144fab3589db2437be13d0517d6a34b58daf6
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-23 21:30:36 +0000

    SubmitTransaction event

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.26;
 
 contract MultiSigWallet {
     event Deposit(address indexed sender, uint256 amount, uint256 balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint256 indexed txIndex,
+        address indexed to,
+        uint256 value,
+        bytes data
+    )
+
+
 }

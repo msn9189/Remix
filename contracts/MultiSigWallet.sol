ceb003ea65e570f734474e421c7b426b2d1a5fbf
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-23 21:44:49 +0000

    function arranged

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -79,21 +79,24 @@ contract MultiSigWallet {
     function submitTransaction(address _to, uint256 _value, bytes memory _data)
         public
         onlyOwner
-        {
-             uint256 txIndex = transactions.length;
+     {
+            uint256 txIndex = transactions.length;
 
-        transactions.push(
-            Transaction({
-                to: _to,
-                value: _value,
-                data: _data,
-                executed: false,
-                numConfirmations: 0
-            })
-        );
+            transactions.push(
+                Transaction({
+                    to: _to,
+                    value: _value,
+                    data: _data,
+                    executed: false,
+                    numConfirmations: 0
+                })
+            );
 
         emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
-        }
+     }
+
+
+     
 
 
 }

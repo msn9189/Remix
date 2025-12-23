44102eabe56f6b487364cf998c2607b895a1bf30
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-23 21:42:27 +0000

    function completed

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -80,7 +80,19 @@ contract MultiSigWallet {
         public
         onlyOwner
         {
-            
+             uint256 txIndex = transactions.length;
+
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
         }
 
 

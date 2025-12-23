7f26d414f8785e42ba4339a5dd613900a26ee0d7
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-23 21:39:18 +0000

    notExecuted modifier

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -41,4 +41,9 @@ contract MultiSigWallet {
         _;
     }
 
+    modifier notExecuted(uint256 _txIndex) {
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+
 }

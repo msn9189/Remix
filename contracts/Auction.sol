31fef8cf37dcbb86adcfa92d1f39d660ac868600
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-25 13:02:06 +0000

    IERC721 interface added to the file

---
+++ contracts/Auction.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+interface IERC721 {
+    function safeTransferFrom(address from, address to, uint256 tokenId)
+        external;
+    function transferFrom(address, address, uint256) external;
+}

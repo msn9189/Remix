cb2f91c66ef6dd051b3bc586e2b720dde89b13b6
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-26 08:25:44 +0000

    IERC20 interface added

---
+++ contracts/CrowdFund.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+interface IERC20 {
+    function transfer(address, uint256) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
+}

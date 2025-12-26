65bb6e13c220fb6f3ad41a8d0096df314fe396f0
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-26 08:26:12 +0000

    CrowdFund contract

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -5,3 +5,7 @@ interface IERC20 {
     function transfer(address, uint256) external returns (bool);
     function transferFrom(address, address, uint256) external returns (bool);
 }
+
+contract CrowdFund {
+    
+}

b26f91ef4cfc203dc34405bc46e817ce0fdcfaf3
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-26 08:27:28 +0000

    unpledge event

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -17,5 +17,6 @@ contract CrowdFund {
 
     event Cancel(uint256 id);
     event Pledge(uint256 indexed id, address indexed caller, uint256 amount);
+    event Unpledge(uint256 indexed id, address indexed caller, uint256 amount);
 
 }

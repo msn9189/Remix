f66f1c283bb4c14561e7ea2abec30d143f590ebd
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-29 07:06:48 +0000

    interface updated

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -9,5 +9,15 @@ interface IERC165 {
 }
 
 interface IERC721 is IERC165 {
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function safeTransferFrom(address from, address to, uint256 tokenId)
+        external;
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes calldata data
+    ) external;
     
 }

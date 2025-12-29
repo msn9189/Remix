9fe8ac5f46f2a2a2d8a787d7fd0386e09757d548
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-29 07:07:28 +0000

    IERC721Receiver interface added

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -31,3 +31,12 @@ interface IERC721 is IERC165 {
         view
         returns (bool);
 }
+
+interface IERC721Receiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}

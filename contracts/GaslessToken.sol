f419cbf32817c96830c370d95bf874b6a4d4f2de
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-29 06:57:27 +0000

    interface implemented

--- contracts/GaslessToken.sol
+++ contracts/GaslessToken.sol
@@ -2,5 +2,26 @@
 pragma solidity ^0.8.26;
 
 interface IERC20Permit {
-    
+   function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount)
+        external
+        returns (bool);
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external; 
 }

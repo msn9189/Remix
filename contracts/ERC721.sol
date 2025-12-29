b1423d2794be5969ed81a0a94f9d40f726d627e2
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-29 07:11:01 +0000

    approve function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -88,5 +88,15 @@ contract ERC721 is IERC721 {
         emit ApprovalForAll(msg.sender, operator, approved);
     }
 
-    
+    function approve(address spender, uint256 id) external {
+        address owner = _ownerOf[id];
+        require(
+            msg.sender == owner || isApprovedForAll[owner][msg.sender],
+            "not authorized"
+        );
+
+        _approvals[id] = spender;
+
+        emit Approval(owner, spender, id);
+    }
 }

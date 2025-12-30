2cf24daa53c1ab2fc3875def4d07094737e1efa5
msn9189 <mehdizadeh.sn@gmail.com>
2025-12-30 13:43:26 +0000

    contract renamed

--- contracts/array.sol
+++ contracts/array.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-contract Array {
+contract practiceArray {
     //Several ways to initialize an array
     uint256[] public arr;
     uint256[] public arr2 = [2,5,9];

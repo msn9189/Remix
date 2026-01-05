7129d676f0db5009cd55cc2a7c452076f4a0b566
msn9189 <mehdizadeh.sn@gmail.com>
2026-01-05 20:49:54 +0000

    struct defined

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -4,5 +4,12 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
-
+    struct Proposal {
+        string description;     
+        uint256 yesVotes;      
+        uint256 noVotes;        
+        uint256 abstainVotes;  
+        uint256 endTime;    
+        bool executed;   
+    }
 }
